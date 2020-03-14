d4f5a2d34b4c89a3a3e520fe42f0f9ff23593859
Edoardo <edo.allari@gmail.com>
2020-03-14 22:49:31 +0100

    Initialization repository

---
+++ src/main/java/com/TicketAssistance/TicketAssistanceApplication.java
@@ -0,0 +1,13 @@
+package com.TicketAssistance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketAssistanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketAssistanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TicketAssistance/TicketAssistanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TicketAssistance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketAssistanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
